bd29c6c1cd0e102083019e21ddb0a646262ebf95
Imanol Perez <iperezadam1B@gmail.com>
2024-02-28 19:55:08 +0100

    Creadas mis 3 ventanas (Imanol)

---
+++ app/src/main/java/com/example/dallyproject/NuevoObjetivo.kt
@@ -0,0 +1,11 @@
+package com.example.dallyproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NuevoObjetivo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nuevo_objetivo)
+    }
+}
