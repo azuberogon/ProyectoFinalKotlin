d2728e94b2c10741b5a38720c29d405b3609db17
ZuberoClase <azuberogondam1b@gmail.com>
2024-02-28 18:54:44 +0100

    nuevas ventanas

---
+++ app/src/main/java/com/example/dallyproject/Calendario.kt
@@ -0,0 +1,11 @@
+package com.example.dallyproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Calendario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendario)
+    }
+}
