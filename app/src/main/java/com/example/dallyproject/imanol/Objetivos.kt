c485ffa0332a6093c4b116d3fa85cec97fbfb6be
HugoUrsa12 <h.urricelqui@gmail.com>
2024-03-07 12:45:33 +0100

    Menu hecho completo con binding y cambiado a fragments parte

--- app/src/main/java/com/example/dallyproject/imanol/Objetivos.kt
+++ app/src/main/java/com/example/dallyproject/imanol/Objetivos.kt
@@ -2,27 +2,43 @@ package com.example.dallyproject.imanol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dallyproject.R
+import com.example.dallyproject.databinding.ActivityObjetivosBinding
 
-class Objetivos : AppCompatActivity() {
-    private lateinit var tematicasAdapter: TematicasObjetivosAdapter
-    private lateinit var recyclerViewTematicas: RecyclerView
+class Objetivos : Fragment(R.layout.activity_objetivos) {
+    //private lateinit var tematicasAdapter: TematicasObjetivosAdapter
+    //private lateinit var recyclerViewTematicas: RecyclerView
+    private lateinit var binding: ActivityObjetivosBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityObjetivosBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_objetivos)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_objetivos, container, false)
+    }
 
         // Supongamos que tienes una lista de temáticas de objetivos
-        val listaTematicas = mutableListOf<TematicaObjetivos>()
-        listaTematicas.add(TematicaObjetivos("Tema 1", 50))
-        listaTematicas.add(TematicaObjetivos("Tema 2", 75))
+        //val listaTematicas = mutableListOf<TematicaObjetivos>()
+        //listaTematicas.add(TematicaObjetivos("Tema 1", 50))
+        //listaTematicas.add(TematicaObjetivos("Tema 2", 75))
         // Añade más temáticas si es necesario
 
-        recyclerViewTematicas = findViewById(R.id.recyclerViewObjetivos)
+        //recyclerViewTematicas = findViewById(R.id.recyclerViewObjetivos)
         // Configurar RecyclerView
-        tematicasAdapter = TematicasObjetivosAdapter(listaTematicas)
-        recyclerViewTematicas.layoutManager = LinearLayoutManager(this)
-        recyclerViewTematicas.adapter = tematicasAdapter
+        //tematicasAdapter = TematicasObjetivosAdapter(listaTematicas)
+        //recyclerViewTematicas.layoutManager = LinearLayoutManager(this)
+        //recyclerViewTematicas.adapter = tematicasAdapter
     }
-}
