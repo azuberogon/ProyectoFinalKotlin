1145bcf26a9cbfdf0befd0d4217eb5f502eb12b0
HugoUrsa12 <h.urricelqui@gmail.com>
2024-03-11 07:20:31 +0100

    Se fini

--- app/src/main/java/com/example/dallyproject/MainActivity.kt
+++ app/src/main/java/com/example/dallyproject/MainActivity.kt
@@ -10,6 +10,7 @@ import com.example.dallyproject.imanol.LocalDatabase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MainActivity : AppCompatActivity() {
@@ -29,20 +30,25 @@ class MainActivity : AppCompatActivity() {
         btnSiguiente = findViewById(R.id.btnLogin)
 
 
-        btnSiguiente.setOnClickListener{
-            val intent = Intent(this, MainActivityInterna::class.java)
-            startActivity(intent)
-//            GlobalScope.launch(Dispatchers.IO) {
-//                val localDB = LocalDatabase.getInstance(this@MainActivity)
-//                val existingUser = localDB.signUpDao().findByUsernameAndPassword(editTxtUsuario.text.toString(), editTxtContraseina.text.toString())
-//                if (existingUser == null) {
-//                    Toast.makeText(this@MainActivity, "El usuario no existe", Toast.LENGTH_SHORT).show()
-//                }else {
-//                    val intent = Intent(this@MainActivity, MainActivityInterna::class.java)
-//                    startActivity(intent)
-//                }
-//            }
+        btnSiguiente.setOnClickListener {
+            val contexto = this@MainActivity
+            GlobalScope.launch(Dispatchers.IO) {
+                val localDB = LocalDatabase.getInstance(contexto)
+                val existingUser = localDB.signUpDao().findByUsernameAndPassword(
+                    editTxtUsuario.text.toString(),
+                    editTxtContraseina.text.toString()
+                )
+                if (existingUser == null) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(contexto, "El usuario no existe", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    val intent = Intent(contexto, MainActivityInterna::class.java)
+                    contexto.startActivity(intent)
+                }
+            }
         }
+
         btnCrearUsuario.setOnClickListener {
             val intent = Intent(this@MainActivity, com.example.dallyproject.imanol.Sign_up::class.java)
             startActivity(intent)
